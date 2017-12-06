5a4e4e44be723d4213184cd7e7a132e7971ba069
Thaddäus Töppen <t.toeppen@gmx.de>
2017-12-06 23:23:31 +0100

    Adding some comments

--- src/Chemostat.hs
+++ src/Chemostat.hs
@@ -1,54 +1,61 @@
-{-# LANGUAGE RecordWildCards #-} -- for passing Pars to ODEs
-{-# LANGUAGE OverloadedStrings #-} -- for Foundation
-{-# LANGUAGE NoImplicitPrelude #-} -- disable standard prelude
+{-# LANGUAGE NoImplicitPrelude    #-} -- disable standard prelude
+{-# LANGUAGE RecordWildCards      #-} -- for passing Pars to ODEs
+{-# LANGUAGE OverloadedStrings    #-} -- for Foundation
 {-# LANGUAGE ExtendedDefaultRules #-} -- for Matplotlib
 
 module Chemostat (runChemostat) where
 
+-- custom functions
 import Util
+
+-- instead of prelude
 import Foundation
 import Foundation.Collection ((!))
+
+-- solving ODEs and working with the resulting data
 import qualified Numeric.LinearAlgebra as LA
 import Numeric.GSL.ODE (ODEMethod(..), odeSolveV)
+
+-- plotting
 import Graphics.Matplotlib
 
+-- to keep type signatures short
 type D = Double
 
--- parameters
+-- parameter data type, holding the parameters that are passed to the differential equations.
 data Par = Par { d, si, gc, xc, kmax, pu, pd, a, gp, kp, xp, gtp, ktp, xtp :: D }
 
+-- the parameters that are actually used in the simulation
 par :: Par
 par = Par {
-  d    = 0.05,
-  si   = 4.0,
-  gc   = 0.5,
-  xc   = 0.3,
-  kmax = 0.3,
-  pu   = 1.0,
-  pd   = 0.1,
-  a    = 0.29,
-  gp   = 0.5,
-  kp   = 0.5,
-  xp   = 0.3,
-  gtp  = 0.5,
-  ktp  = 0.05,
-  xtp  = 0.3
+  d    = 0.05, -- dilution rate
+  si   = 4.0,  -- inflow nutrient concentration
+  gc   = 0.5,  -- maximum consumption rate Cu/Cd
+  xc   = 0.3,  -- conversion efficiency Cu/Cd 
+  kmax = 0.3,  -- maximim half saturation Cu/Cd
+  pu   = 1.0,  -- palatability Cu (undefended)
+  pd   = 0.1,  -- palatability Cd (defended)
+  a    = 0.29, -- trade-off strength
+  gp   = 0.5,  -- maximum consumption rate Pi
+  kp   = 0.5,  -- half saturation Pi
+  xp   = 0.3,  -- conversion efficiency Pi
+  gtp  = 0.5,  -- maximum consumption rate Pt
+  ktp  = 0.05, -- half saturation Pt
+  xtp  = 0.3   -- conversion effienciency Pt
 } 
 
--- initial values
-initVals :: LA.Vector D
-initVals = LA.fromList [ 0.5, 0.5, 0.5, 0.2, 0.0 ]
+-- initial values for all variables
+initVals :: LA.Vector D -- [   S,  Cu,  Cd,  Pi,  Pt ]
+initVals =  LA.fromList    [ 0.5, 0.5, 0.5, 0.2, 0.0 ]
 
--- sub functions
-kcu', kcd' :: Par -> D
-kcu' Par{..} = kmax - a*pu
-kcd' Par{..} = kmax - a*pd
-
+-- helpers for sub-functions (half saturation/palatability trade-off)
 kcu, kcd :: D
 kcu = kcu' par
+  where kcu' Par{..} = kmax - a*pu -- trade-off curve Cu
 kcd = kcd' par
+  where kcd' Par{..} = kmax - a*pd -- trade-off curve Cd
 
--- differential equations
+-- the differential equations to solve
 ds, dcu, dcd, dp, dtp :: Par -> D -> D -> D -> D -> D -> D
 ds   Par{..} s c1 c2 p tp = d*(si - s) - s*(gc*c1/(kcu + s) + (gc*c2/(kcd + s)))
 dcu  Par{..} s c1 c2 p tp = c1*(xc*gc*s/(kcu + s) - gp*pu*p/(kp + pu*c1 + pd*c2) - d)
@@ -56,20 +63,22 @@ dcd  Par{..} s c1 c2 p tp = c2*(xc*gc*s/(kcd + s) - gp*pd*p/(kp + pu*c1 + pd*c2)
 dp   Par{..} s c1 c2 p tp = p*(xp*gp*((pu*c1 + pd*c2)/(kp + pd*c1 + pd*c2)) - gtp*tp/(ktp + p) - d)
 dtp  Par{..} s c1 c2 p tp = tp*(xtp*gtp*p/(ktp + p) - d)
 
+-- the differential equations system in the form that is passed to the solver
 eqSystem :: D -> LA.Vector D -> LA.Vector D
 eqSystem t vars = LA.fromList [ ds  par s c1 c2 p tp
                               , dcu par s c1 c2 p tp
                               , dcd par s c1 c2 p tp
                               , dp  par s c1 c2 p tp
                               , dtp par s c1 c2 p tp ]
-  where
-    [s, c1, c2, p, tp] = LA.toList vars
+  where 
+    -- pattern matching on vars to get the population densities for using them in the functions body
+    [s, c1, c2, p, tp] = LA.toList vars 
 
 -- the time steps for which the result is given
 time ::  LA.Vector D
-time = times 0 1000 0.1
+time = times 0 2000 0.1
 
--- the solutions matrix
+-- solving the equations numerically; returning the solutions matrix
 solution :: LA.Matrix D
 solution = odeSolveV
   RKf45    -- ODE Method
@@ -80,9 +89,12 @@ solution = odeSolveV
   initVals -- inital conditions
   time     -- desired solution times
 
+-- adding a column to the solution matrix, containing the total of both clones Cu+Cd
 solWithCloneTotal :: LA.Matrix D
 solWithCloneTotal = matrixAddSumCol 1 2 solution
 
+-- defining a plot that is later saved to a file
+-- this uses python with matplotlib under the hood
 plot1 :: Matplotlib
 plot1 =
   plot time (LA.toColumns solWithCloneTotal ! 0) @@ [o1 "-", o2 "linewidth" 1, o2 "label" "S"] %
@@ -93,6 +105,7 @@ plot1 =
   plot time (LA.toColumns solWithCloneTotal ! 5) @@ [o1 "-", o2 "linewidth" 1, o2 "label" "Ct"] %
   legend @@ [o2 "fancybox" True, o2 "shadow" False, o2 "title" "Legend", o2 "loc" "upper left"]
 
+-- putting everyting together to export it for usage in Main.hs
 runChemostat :: IO ()
 runChemostat = do
   Right _ <- file "plot1.pdf" plot1
